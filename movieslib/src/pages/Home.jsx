31502fff1934886893fe6b0a6d1b41aade8062dc
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-24 21:44:53 -0300

    Buscando Filmes na home

--- movieslib/src/pages/Home.jsx
+++ movieslib/src/pages/Home.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovie = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+    console.log(topMovies)
+  }
+
+  useEffect(() => {
+    
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+    getTopRatedMovie(topRatedUrl)
+
+  }, [])
+
   return (
     <div>Home</div>
   )
