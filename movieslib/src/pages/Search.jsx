a08c814b4aecd222beed0aa12381392a997732d4
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-25 20:35:36 -0300

    Página de busca

--- movieslib/src/pages/Search.jsx
+++ movieslib/src/pages/Search.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+// CSS
+import './MoviesGrid.css'
+
+import { useState, useEffect } from 'react'
+
+// Router
+import { useSearchParams } from 'react-router-dom'
+
+// Components
+import MovieCard from '../components/MovieCard'
+
+const searchUrl = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+console.log(apiKey)
 
 const Search = () => {
+  const [ searchParams ] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+
+  const query = searchParams.get('q')
+
+  const getSearchedMovies = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setMovies(data.results)
+  }
+
+  useEffect(() => {
+    
+    const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`
+    getSearchedMovies(searchWithQueryUrl)
+
+  }, [query])
+
   return (
-    <div>Search</div>
+    <div className='container'>
+      <h2 className='title'>
+        Resultados para a busca: <span className='query-text'>{query}</span>
+        </h2>
+      <div className="movies-container">
+        {movies && movies.length === 0 && <p>Carregando ...</p>}
+          {movies && movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie}/>
+          ))}
+      </div>
+    </div>
   )
 }
 
