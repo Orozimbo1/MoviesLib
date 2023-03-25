395ac5b185edaeae731d6eca5162f3e47b561213
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-24 21:12:20 -0300

    Setup do projeto

--- movieslib/src/main.jsx
+++ movieslib/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Router 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
 
+// pages
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import Search from './pages/Search'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='seach' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
