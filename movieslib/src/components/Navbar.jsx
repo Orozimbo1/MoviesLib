543e64fa5b785a668dfab41b18b1b6158c4da26d
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-24 21:31:26 -0300

    Estilos globais

--- movieslib/src/components/Navbar.jsx
+++ movieslib/src/components/Navbar.jsx
@@ -1,13 +1,23 @@
+// Router
 import { Link } from 'react-router-dom'
+
+// Icons
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 const Navbar = () => {
   return (
     <nav id='navbar'>
         <h2>
-          <Link to='/'>MoviesLib</Link>
+          <Link to='/'>
+            <BiCameraMovie /> MoviesLib
+          </Link>
         </h2>
-        <Link to='/movie/1'>Movie</Link>
-        <Link to='/search'>Search</Link>
+        <form>
+          <input type="text" placeholder='Busque um filme' />
+          <button type='submit'>
+            <BiSearchAlt2 />
+          </button>
+        </form>
     </nav>
   )
 }
