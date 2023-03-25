d491e518d6f9d972ce525bdcdf78397d66f15bea
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-25 20:08:28 -0300

    Implementando a função de search na navbar

--- movieslib/src/components/Navbar.jsx
+++ movieslib/src/components/Navbar.jsx
@@ -2,12 +2,26 @@
 import './Navbar.css'
 
 // Router
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { useState } from 'react'
 
 // Icons
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 const Navbar = () => {
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
   return (
     <nav id='navbar'>
         <h2>
@@ -15,8 +29,12 @@ const Navbar = () => {
             <BiCameraMovie /> MoviesLib
           </Link>
         </h2>
-        <form>
-          <input type="text" placeholder='Busque um filme' />
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" placeholder='Busque um filme' 
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
           <button type='submit'>
             <BiSearchAlt2 />
           </button>
